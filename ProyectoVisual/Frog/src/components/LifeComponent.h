7ae0b8a87792ef7b6eb1399c49dc681433a36696
Jiale <jialehe28@gmail.com>
2024-02-19 16:23:18 +0100

    Life Component metido

---
+++ ProyectoVisual/Frog/src/components/LifeComponent.h
@@ -0,0 +1,16 @@
+#pragma once
+class LifeComponent
+{
+private:
+	int vidaActual = 0; //Un  punto es media mosca
+	int vidaMaxima = 0;
+public:
+	LifeComponent() {};
+	LifeComponent(int actual, int maxima) :vidaActual(actual), vidaMaxima(maxima) {};
+	int GetActual();
+	int GetMax();
+	void SetActual(int n);
+	void SetMax(int n);
+	bool alive();
+};
+
