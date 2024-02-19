ac416f76eb248ba2ecedbd32e572e877b958c722
Erisolo <marsolor@ucm.es>
2024-02-19 16:38:16 +0100

    input manager de diego y mitad de clase entity

--- ProyectoVisual/Frog/src/ecs/Entity.cpp
+++ ProyectoVisual/Frog/src/ecs/Entity.cpp
@@ -1,1 +1,14 @@
 #include "Entity.h"
+#include "Component.h"
+void
+Entity::update() {
+	
+	for (auto it = Componentes.begin(); it != Componentes.end(); ++it)
+	{
+		it->second->update();
+	}
+}
+
+void Entity::Render()
+{
+}
--- ProyectoVisual/Frog/src/managers/InputManager.h
+++ ProyectoVisual/Frog/src/managers/InputManager.h
@@ -1,9 +1,50 @@
 #pragma once
+#include <SDL.h>
 /*
 *Encargado de capturar los eventos de entrada de controles del juego y enviarlas a las entidades pertinentes.
 * 
+* Para hacerla me he inspirado en los ejemplos de Samir.
+* Diego Montenegro Calvo.
 */
 class InputManager
 {
-};
+private:
+    //Definen los distintos botones que pueden estar pulsados o no
+    enum state {
+        BTN_ACTION1,
+        BTN_ACTION2,
+        BTN_ACTION3,
+        BTN_UP,
+        BTN_DOWN,
+        BTN_LEFT,
+        BTN_RIGHT,
+        BTN_ESCAPE,
+        END //Para tener un valor con el que crear y utilizar el array
+    };
+    bool states[END];
+    //Constructor vacío privado para poder instanciar desde dentro
+    InputManager() {};
+    ~InputManager() {};
+    static InputManager* instance;
+    //Actualiza el estado de los eventos
+    void UpdateStates(const SDL_Event& event);
+    //Establece todos los estados a false
+    void ClearStates();
+public:
+    InputManager(InputManager &im) = delete;
+    void operator=(const InputManager&) = delete;
+    /*
+    *Devuelve una instancia de InputManager, en caso de no haber una, la crea 
+    */
+    static InputManager* GetInstance();
+    void PollEvents();
+    bool getAction1();
+    bool getAction2();
+    bool getAction3();
+    bool getUp();
+    bool getDown();
+    bool getLeft();
+    bool getRight();
 
+
+};
