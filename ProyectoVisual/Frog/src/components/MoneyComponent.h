d0670d90ee4dabe93732f73761223054f7ec294f
Jiale <jialehe28@gmail.com>
2024-02-21 16:33:41 +0100

    Comntarios añadidos

--- ProyectoVisual/Frog/src/components/MoneyComponent.h
+++ ProyectoVisual/Frog/src/components/MoneyComponent.h
@@ -6,7 +6,7 @@ private:
 public:
 	MoneyComponent() :money(0) {};
 	MoneyComponent(int n) :money(n) {};
-	int GetMoney();
-	void AddMoney(int n);
-	bool TakeMoney(int price);
+	int GetMoney(); 
+	void AddMoney(int n); //Metodo para añadirle dinero a nuestro total
+	bool TakeMoney(int price); //El metodo es un booleano por haber casos en donde no tengamos suficiente dinero.
 };
