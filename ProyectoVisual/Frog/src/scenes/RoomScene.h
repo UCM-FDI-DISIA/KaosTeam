5ca77aad1a95cb65c20ffda3dcbd3dc59854ca3c
DiegoAlonsoValenciano <125390412+DiegoAlonsoValenciano@users.noreply.github.com>
2024-02-28 17:19:30 +0100

    la mosca va a la rana

--- ProyectoVisual/Frog/src/scenes/RoomScene.h
+++ ProyectoVisual/Frog/src/scenes/RoomScene.h
@@ -3,6 +3,7 @@
 //#include "../ecs/Entity.h"
 #include"../components/MovementComponentFly.h"
 #include "../components/RenderComponent.h"
+#include "../components/FollowPlayerComponent.h"
 
 class RoomScene : public Scene
 {
@@ -11,14 +12,15 @@ private:
 	std::vector<Entity*> entityList;
 	MapManager* mapReader;
 	int id;
+	Entity* player = nullptr;
 public:
 	RoomScene(int id) : id(id) {
 		//A trav�s del id de la sala, se deben buscar los datos necesarios para cargar el tilemap y las entidades de la sala.
 		mapReader = new MapManager("tileMap_Prueba");
 		mapReader->load("resources/maps/tileMap_Prueba.tmx", sdlutils().renderer());
 
-		Entity* player = new Entity(this);
-		Vector2D v(2, 2);
+		player = new Entity(this);
+		Vector2D v(0, 0);
 		player->addComponent(MOVEMENT_COMPONENT, new MovementComponent(v));
 		RenderComponent* rndr = new RenderComponent("../Frog/resources/sprites/ranaSpritesheet.png", 4, 4);
 		rndr->setContext(player);
@@ -37,6 +39,16 @@ public:
 		fly->addRenderComponent(rndr);
 		entityList.push_back(fly);
 
+		Entity* flyToPlayer = new Entity(this);
+		FollowPlayerComponent* fpc = new FollowPlayerComponent(Vector2D(5, 5));
+		fpc->setContext(flyToPlayer);
+		flyToPlayer->addComponent(MOVEMENT_COMPONENT, fpc);
+
+		rndr = new RenderComponent("../Frog/resources/sprites/moscaSpritesheet.png", 1, 3, 0.5);
+		rndr->setContext(flyToPlayer);
+		flyToPlayer->addRenderComponent(rndr);
+		entityList.push_back(flyToPlayer);
+
 	};
 
 	void AddEntity(Entity* entity);
@@ -44,5 +56,6 @@ public:
 	void Update() override;
 	virtual ~RoomScene();
 	MapManager* getMapReader() { return mapReader; };
+	Entity* getPlayer() { return player; };
 
 };
