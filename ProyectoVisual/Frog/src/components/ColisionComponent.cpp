21312137a1f218b9cd2f8add0461a72d437cb1c8
Mauro Martínez <maumar03@ucm.es>
2024-03-03 22:31:48 +0100

    Lo basico

---
+++ ProyectoVisual/Frog/src/components/ColisionComponent.cpp
@@ -0,0 +1,9 @@
+#include "ColisionComponent.h"
+#include <iostream>
+
+bool ColisionComponent::CheckTile(Vector2D TileToCheck)
+{
+	// checks
+	
+	return false;
+}
---
+++ ProyectoVisual/Frog/src/components/ColisionComponent.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "../ecs/Component.h"
+#include "../utils/Vector2D.h"
+
+class ColisionComponent
+{
+	bool CheckTile(Vector2D tileToCheck);
+
+};
+
