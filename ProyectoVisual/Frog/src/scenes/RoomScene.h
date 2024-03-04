4d8177f84b881ac47c2d77aaa5c6916732c8e2a9
mikeletzu <120392721+mikeletzu@users.noreply.github.com>
2024-03-04 21:30:46 +0100

    Map layers añadido pero follow error

--- ProyectoVisual/Frog/src/scenes/RoomScene.h
+++ ProyectoVisual/Frog/src/scenes/RoomScene.h
@@ -18,37 +18,26 @@ public:
 	RoomScene(int id) : id(id) {
 		//A trav�s del id de la sala, se deben buscar los datos necesarios para cargar el tilemap y las entidades de la sala.
 		mapReader = new MapManager("resources/maps/H1map.tmx", this);
-
-		player = new Entity(this);
-		Vector2D v(2, 2);
-		player->addComponent(MOVEMENT_COMPONENT, new MovementComponentFrog(v));
-		RenderComponent* rndr = new RenderComponent("../Frog/resources/sprites/ranaSpritesheet.png", 4, 4);
-		rndr->setContext(player);
-		player->addRenderComponent(rndr);
-		entityList.push_back(player);
-
-
-
+		
 		Entity* fly = new Entity(this);
 		MovementComponentFly* mvm = new MovementComponentFly(Vector2D(2, 3));
 		mvm->setContext(fly);
 		fly->addComponent(MOVEMENT_COMPONENT, mvm);
-
-		rndr = new RenderComponent("../Frog/resources/sprites/moscaSpritesheet.png", 1, 3, 0.5);
+		RenderComponent* rndr = new RenderComponent("../Frog/resources/sprites/moscaSpritesheet.png", 1, 3, 0.5);
 		rndr->setContext(fly);
 		fly->addRenderComponent(rndr);
 		entityList.push_back(fly);
 
+		/*
 		Entity* flyToPlayer = new Entity(this);
 		FollowPlayerComponent* fpc = new FollowPlayerComponent(Vector2D(0, 0));
 		fpc->setContext(flyToPlayer);
 		flyToPlayer->addComponent(MOVEMENT_COMPONENT, fpc);
-
 		rndr = new RenderComponent("../Frog/resources/sprites/moscaSpritesheet.png", 1, 3, 0.5);
 		rndr->setContext(flyToPlayer);
 		flyToPlayer->addRenderComponent(rndr);
 		entityList.push_back(flyToPlayer);
-
+		*/
 	};
 
 	void AddEntity(Entity* entity);
