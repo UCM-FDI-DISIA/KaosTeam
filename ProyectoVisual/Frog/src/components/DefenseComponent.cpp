5aaf1c72a7323fbdda9400264f6db634428970c6
MauroUCM <maumar03@ucm.es>
2024-03-18 16:54:46 +0100

    Acabado.

--- ProyectoVisual/Frog/src/components/DefenseComponent.cpp
+++ ProyectoVisual/Frog/src/components/DefenseComponent.cpp
@@ -4,19 +4,35 @@
 #include "../managers/DataManager.h"
 #include "RenderComponentFrog.h"
 
+// faltan animaciones e implementarlo en el daño
+
 void DefenseComponent::update()
 {
-	if (!defenseActive && auxInput->getAction4())
+	if (!otherStatesChecker() && auxInput->getAction4() && (DataManager::GetInstance()->getFrameTime() - timeSinceChange) > defenseCooldown)
 	{
-		
+		defenseActive = true;
+		timeSinceChange = DataManager::GetInstance()->getFrameTime();
 	}
-	else if (defenseActive)
+	else if (defenseActive && (DataManager::GetInstance()->getFrameTime() - timeSinceChange) > defenseTime)
 	{
-		
+			defenseActive = false;
+			timeSinceChange = DataManager::GetInstance()->getFrameTime();
 	}
 }
 
-bool DefenseComponent::getDefenseActive()
+bool DefenseComponent::getDefenseActive()	// getter de defenseActive para comprobar cuando reciba daño
 {
 	return defenseActive;
+}
+
+bool DefenseComponent::otherStatesChecker()	// comprueba que no haya otras acciones o estados en curso
+{
+	bool aux = false;
+
+	if(defenseActive)	// añadir detector de ataque con lengua y ampliar si fuese necesario
+	{
+		aux = true;
+	}
+
+	return aux;
 }
--- ProyectoVisual/Frog/src/components/DefenseComponent.h
+++ ProyectoVisual/Frog/src/components/DefenseComponent.h
@@ -6,10 +6,11 @@ class DefenseComponent : public Component
 {
 private:
 	bool defenseActive = false;
-	int defenseTime = 50, timeSinceActivation = 0, defenseCooldown = 20;
+	int defenseTime = 50, timeSinceChange = 0, defenseCooldown = 20;
 	InputManager* auxInput;
 
 public:
 	void update() override;
 	bool getDefenseActive();
+	bool otherStatesChecker();
 };
