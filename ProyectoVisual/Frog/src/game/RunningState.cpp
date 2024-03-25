e019bb9c6b7b928bae5d78ce29a2ff3f93fa4d7b
Jiale <jialehe28@gmail.com>
2024-03-25 20:13:01 +0100

    Un pequeño placeholder para como seria el cambio de rooms

--- ProyectoVisual/Frog/src/game/RunningState.cpp
+++ ProyectoVisual/Frog/src/game/RunningState.cpp
@@ -4,6 +4,7 @@
 
 RunningState::RunningState(Game* game) : hud(new HUDManager()), currScene(new RoomScene(1))
 {
+	allRooms.reserve(numRooms);
 }
 
 RunningState::~RunningState()
@@ -16,8 +17,23 @@ void RunningState::leave()
 
 void RunningState::update()
 {
+	currScene->update();
 }
 
 void RunningState::enter()
 {
+}
+
+void RunningState::changeScene(int id)
+{
+	if (allRooms[id] == nullptr)
+	{
+		currScene = allRooms[id];
+	}
+	else
+	{
+		allRooms[id] = new RoomScene(id);
+	}
+ //Tenemos de encontrar una forma de entrar en nuevas habitaciones aquí
+
 }
--- ProyectoVisual/Frog/src/game/RunningState.h
+++ ProyectoVisual/Frog/src/game/RunningState.h
@@ -4,7 +4,8 @@
 #include "../managers/HUDManager.h"
 #include "../ecs/Scene.h"
 #include "Game.h"
-
+#include <vector>
+const int numRooms = 100;
 class RunningState: public GameState
 {
 public:
@@ -13,13 +14,14 @@ public:
 	void leave() override;
 	void update() override;
 	void enter() override;
-
+	void changeScene(int id); //Este metodo se usará para el cambio de habitaciones en el estado;
 	//Getters
 	Scene* getScene() const override { return currScene; };
 	HUDManager* getHUD()const { return hud; }
 private:
 	HUDManager* hud; //Puntero que gestiona la pantalla de MenuInicio
 	Scene* currScene; //puede haber escenas d menu d opciones,etc.
+	vector<Scene*> allRooms;
 	//void se presiona el boton d pausa {llama al game change estate a ause y le pasa la currScene y el hud
 };
 
