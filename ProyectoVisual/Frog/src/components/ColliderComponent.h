79cb300162f4df913cb2aeec8066bd8069b47c27
Diego Montenegro <dmonoficial22@gmail.com>
2024-03-27 18:59:01 +0100

    collider component creado (ahora toca hacerlo)

---
+++ ProyectoVisual/Frog/src/components/ColliderComponent.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../ecs/Component.h"
+#include "../utils/Vector2D.h"
+
+//Comprueba colisiones de objetos, y envía un mensaje con la información relevante a los componentes suscritos
+//La llamada a comporbar colisiones debe hacerse en el update del room antes o después del update de las entidades
+class ColliderComponent : public Component
+{
+public:
+	ColliderComponent() {};
+	~ColliderComponent() {};
+};
+
