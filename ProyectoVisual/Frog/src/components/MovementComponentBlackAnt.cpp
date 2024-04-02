1d49366be9eafcfc13d71d3f5ae784ab1c966f45
xtina2434 <crmuno10@ucm.es>
2024-04-02 20:02:10 +0200

    faltan las animaciones de la hormiga

--- ProyectoVisual/Frog/src/components/MovementComponentBlackAnt.cpp
+++ ProyectoVisual/Frog/src/components/MovementComponentBlackAnt.cpp
@@ -6,64 +6,62 @@ void MovementComponentBlackAnt::update() {
 
 	if (!waitToAttack && !waitToMove && !isMoving && (DataManager::GetInstance()->getFrameTime() - lastTimeMoved) > waitTime) {
 		lastTimeMoved = DataManager::GetInstance()->getFrameTime();
-			
+		playerPosition = targetMovementComp->getPosition();
 		switch (actualDirection)
 		{
 		case RIGHT: {
 			if (isAtacking) {
-				velocity = Vector2D(range, 0);
+				velocity = Vector2D(diff, 0);
 				//anim->playAnimation("RIGHT");
-				isAtacking = false;
+				framesPerMove = 2 + velocity.magnitude() * 3;
 			}
 			else {
 				velocity = Vector2D(1, 0);
 				//anim->playAnimation("RIGHT");
+				framesPerMove = 4 + velocity.magnitude() * 3;
 			}
-			framesPerMove = 4 + velocity.magnitude() * 3;
 		}
 		break;
 		case LEFT:
 		{
 			if (isAtacking) {
-				velocity = Vector2D(-range, 0);
+				velocity = Vector2D(diff, 0);
 				//anim->playAnimation("LEFT");
-				isAtacking = false;
+				framesPerMove = 2 + velocity.magnitude() * 3;
 			}
 			else {
 				velocity = Vector2D(-1, 0);
 				//anim->playAnimation("LEFT");
+				framesPerMove = 4 + velocity.magnitude() * 3;
 			}
-			framesPerMove = 4 + velocity.magnitude() * 3;
-			
 		}
 		break;
 		case UP:
 		{
 			if (isAtacking) {
-				velocity = Vector2D(0, -range);
+				velocity = Vector2D(0, diff);
 				//anim->playAnimation("LEFT");
-				isAtacking = false;
+				framesPerMove = 2 + velocity.magnitude() * 3;
 			}
 			else {
 				velocity = Vector2D(0, -1);
 				//anim->playAnimation("LEFT");
+				framesPerMove = 4 + velocity.magnitude() * 3;
 			}
-			framesPerMove = 4 + velocity.magnitude() * 3;
 		}
 		break;
 		case DOWN:
 		{
 			if (isAtacking) {
-				velocity = Vector2D(0, range);
+				velocity = Vector2D(0, diff);
 				//anim->playAnimation("LEFT");
-				isAtacking = false;
+				framesPerMove = 4 + velocity.magnitude() * 3;;
 			}
 			else {
 				velocity = Vector2D(0, 1);
 				//anim->playAnimation("LEFT");
+				framesPerMove = 4 + velocity.magnitude() * 3;
 			}
-			framesPerMove = 4 + velocity.magnitude() * 3;
-
 		}
 		break;
 		default:
@@ -71,44 +69,55 @@ void MovementComponentBlackAnt::update() {
 		}
 		isMoving = true;
 	}
-	else if (isMoving && (DataManager::GetInstance()->getFrameTime() - lastTimeMoved) > movementFrameRate) {
+	else if (isMoving && (DataManager::GetInstance()->getFrameTime() - lastTimeMoved) > movementFrameRate)
+	{
 		lastTimeMoved = DataManager::GetInstance()->getFrameTime();
-	
 		int t = ent->getScene()->getMapReader()->getTileSize();
 		framesMoved++;
-		offsetInCasilla.setX(offsetInCasilla.getX() + t / framesPerMove * velocity.getX());
+
+		if (actualDirection == RIGHT || actualDirection == LEFT) {
+			offsetInCasilla.setX(offsetInCasilla.getX() + t / framesPerMove * velocity.getX());
+		}
+		else {
+			offsetInCasilla.setY(offsetInCasilla.getY() + t / framesPerMove * velocity.getY());
+		}
 
 		if (offsetInCasilla.getX() * velocity.normalize().getX() >= t / 2 ||
-			offsetInCasilla.getY() * velocity.normalize().getY() >= t / 2) //si se mueve mas de media casilla, est� en la casilla siguiente
+			offsetInCasilla.getY() * velocity.normalize().getY() >= t / 2)
 		{
 			changePos(velocity.normalize() + posCasilla);
 			if (actualDirection == LEFT || actualDirection == RIGHT)
 				offsetInCasilla.setX(offsetInCasilla.getX() * -1);
-			else
+			else if (actualDirection == UP || actualDirection == DOWN)
 				offsetInCasilla.setY(offsetInCasilla.getY() * -1);
 		}
 		if (framesMoved == framesPerMove) {
-			//posCasilla = destCasilla;
 			offsetInCasilla = { 0,0 };
 			framesMoved = 0;
 			isMoving = false;
-			changeDirection();
-			isPlayerNear();
-			checkCollisionWall();
+			if (isAtacking) {
+				diff = 0;
+				isAtacking = false;
+				checkCollisionWall();
+			}
+			bool attack = isPlayerNear();
+			if (!attack)
+				changeDirection();
 		}
 	}
 	else if (waitToAttack) {
-		lastTimeMoved = DataManager::GetInstance()->getFrameTime();
-		if ((DataManager::GetInstance()->getFrameTime() - lastTimeMoved) > waitTimeAttack) {
+		Uint32 currentTime = DataManager::GetInstance()->getFrameTime();
+		if ((currentTime - lastTimeMoved) > waitTimeAttack) {
 			isAtacking = true;
 			waitToAttack = false;
+			lastTimeMoved = currentTime;
 		}
-
 	}
 	else if (waitToMove) {
-		lastTimeMoved = DataManager::GetInstance()->getFrameTime();
-		if ((DataManager::GetInstance()->getFrameTime() - lastTimeMoved) > immobileTime) {
+		Uint32 currentTime = DataManager::GetInstance()->getFrameTime();
+		if ((currentTime - lastTimeMoved) > immobileTime) {
 			waitToMove = false;
+			lastTimeMoved = currentTime;
 		}
 	}
 }
@@ -118,45 +127,89 @@ void MovementComponentBlackAnt::changeDirection() {
 	//cambiar direccion
 	switch (newDirection)
 	{
-		case 0: actualDirection = RIGHT;
-			break;
-		case 1: actualDirection = LEFT;
-			break;
-		case 2: actualDirection = UP;
-			break;
-		case 3: actualDirection = DOWN;
-			break;
-		default:
-			break;
+	case 0: {
+		Vector2D aux = posCasilla + Vector2D(1, 0);
+		if (checkIfTileWalkable(aux)) {
+			actualDirection = RIGHT;
+		}
+		else
+			changeDirection();
 	}
-}
-void MovementComponentBlackAnt::isPlayerNear() {
-	//cambiar direccion
-	switch (actualDirection)
-	{
-	case RIGHT: {
-		if (playerPosition.getX() - posCasilla.getX() <= 3) waitToAttack = true;
+		  break;
+	case 1: {
+		Vector2D aux = posCasilla + Vector2D(-1, 0);
+		/*if (checkIfTileWalkable(aux)) {
+			actualDirection = LEFT;
+		}*/
+		if (aux.getX() >= 0) {
+			actualDirection = LEFT;
+		}
+		else
+			changeDirection();
 	}
-		break;
-	case LEFT: {
-		if (posCasilla.getX() - playerPosition.getX()  <= 3) waitToAttack = true;
+		  break;
+	case 2: {
+		Vector2D aux = posCasilla + Vector2D(0, -1);
+		/*if (checkIfTileWalkable(aux)) {
+			actualDirection = UP;
+		}*/
+		if (aux.getY() >= 0) {
+			actualDirection = UP;
+		}
+		else
+			changeDirection();
+
 	}
-		break;
-	case UP: {
-		if (posCasilla.getY() - playerPosition.getY() <= 3) waitToAttack = true;
+		  break;
+	case 3: {
+		Vector2D aux = posCasilla + Vector2D(0, 1);
+		if (checkIfTileWalkable(aux)) {
+			actualDirection = DOWN;
+		}
+		else
+			changeDirection();
 	}
-		break;
-	case DOWN: {
-		if (playerPosition.getY() - posCasilla.getY()  <= 3) waitToAttack = true;
-	}
-		break;
+		  break;
 	default:
 		break;
 	}
-	if (waitToAttack) {
-		std::cout << "he detectado jugador";
+}
+bool MovementComponentBlackAnt::isPlayerNear() {
+	if (playerPosition.getY() == posCasilla.getY() && playerPosition.getX() - posCasilla.getX() <= range) {
+		waitToAttack = true;
+		diff = playerPosition.getX() - posCasilla.getX();
+		if (diff > 0)
+			actualDirection = RIGHT;
+		else if (diff < 0)
+			actualDirection = LEFT;
 	}
+	else if (!waitToAttack && playerPosition.getX() == posCasilla.getX() && playerPosition.getY() - posCasilla.getY() <= range) {
+		waitToAttack = true;
+		isAtacking = true;
+		diff = playerPosition.getY() - posCasilla.getY();
+		if (diff > 0)
+			actualDirection = DOWN;
+
+		else if (diff < 0)
+			actualDirection = UP;
+	}
+	return waitToAttack;
 }
 void MovementComponentBlackAnt::checkCollisionWall() {
 	//comprobar si da con la pared y si es asi waitToMove = true
+	if (actualDirection == RIGHT && !checkIfTileWalkable(posCasilla + Vector2D(1, 0)))
+		waitToMove = true;
+	else if (actualDirection == LEFT) {
+		Vector2D aux = posCasilla + Vector2D(-1, 0);
+		if (aux.getX() < 0)
+			waitToMove = true;
+	} /* && !checkIfTileWalkable(posCasilla + Vector2D(-1, 0)))*/
+	else if (actualDirection == DOWN && !checkIfTileWalkable(posCasilla + Vector2D(0, 1)))
+		waitToMove = true;
+	else if (actualDirection == UP) {
+		Vector2D aux = posCasilla + Vector2D(0, -1);
+		if (aux.getY() < 0) {
+			waitToMove = true;
+		}
+	}/* && !checkIfTileWalkable(posCasilla + Vector2D(0, -1)))*/
 }
--- ProyectoVisual/Frog/src/components/MovementComponentBlackAnt.h
+++ ProyectoVisual/Frog/src/components/MovementComponentBlackAnt.h
@@ -3,6 +3,7 @@
 #include "../sdlutils/SDLUtils.h"
 #include "../managers/DataManager.h"
 #include "AnimationComponent.h"
+#include "MovementComponentFrog.h"
 
 class MovementComponentBlackAnt : public MovementComponent {
 private:
@@ -10,22 +11,24 @@ private:
 	Uint32  waitTime, lastTimeMoved, movementFrameRate, //tiempo de espera (frames) para el movimiento entre casillas
 		framesPerMove, framesMoved, waitTimeAttack, immobileTime;
 	Direction actualDirection;		//direccion actual				
-	//int casillaActual;				//casilla en la que se encuentra
 	bool isMoving;
 	bool isAtacking;
 	bool waitToAttack;
 	bool waitToMove;
 	int range;
+	float diff;
+	MovementComponentFrog* targetMovementComp;
 	Vector2D playerPosition;
 	AnimationComponent* anim;
 	RandomNumberGenerator& rand_;	//generador de numeros random
 	void changeDirection();
-	void isPlayerNear();
+	bool isPlayerNear();
 	void checkCollisionWall();
 public:
-	MovementComponentBlackAnt(Vector2D casilla, AnimationComponent* a, Vector2D playerPos) : MovementComponent(casilla), lastTimeMoved(SDL_GetTicks()), anim(a), playerPosition(playerPos), rand_(sdlutils().rand())
+	MovementComponentBlackAnt(Vector2D casilla, AnimationComponent* a, MovementComponentFrog* target) : MovementComponent(casilla), lastTimeMoved(SDL_GetTicks()), anim(a), targetMovementComp(target), rand_(sdlutils().rand())
 	{
 		actualDirection = RIGHT;
+		playerPosition = targetMovementComp->getPosition();
 		//anim->playAnimation("RIGHT");
 		waitTime = 1000;
 		movementFrameRate = 50;
@@ -38,6 +41,7 @@ public:
 		range = 3;
 		waitToAttack = false;
 		waitToMove = false;
+		diff = 0;
 	};
 	void update() override;
 };
