a37626552596319e679c510080dadf3143216be1
MauroUCM <maumar03@ucm.es>
2024-04-15 16:57:47 +0200

    meh

--- ProyectoVisual/Frog/src/components/RenderComponentDestructible.cpp
+++ ProyectoVisual/Frog/src/components/RenderComponentDestructible.cpp
@@ -1,1 +1,5 @@
 #include "RenderComponentDestructible.h"
+#include "RenderComponentFrog.h"
+#include "../scenes/RoomScene.h"
+#include "../managers/MapManager.h"
+#include "TransformComponent.h"
--- ProyectoVisual/Frog/src/components/RenderComponentDestructible.h
+++ ProyectoVisual/Frog/src/components/RenderComponentDestructible.h
@@ -1,5 +1,18 @@
 #pragma once
+#include "../ecs/Component.h"
+#include "../sdlutils/Texture.h"
+#include "../sdlutils/SDLUtils.h"
+class TransformComponent;
+
 class RenderComponentDestructible
 {
+private:
+
+
+public:
+
+
+
+
 };
 
