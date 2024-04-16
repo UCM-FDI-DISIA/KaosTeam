ad22dfa486b6f5630854541abfbe394e84a26497
luisja30 <luisjnav@ucm.es>
2024-04-16 20:24:24 +0200

    Bomba se mueve

--- ProyectoVisual/Frog/src/components/MovementComponentBomb.cpp
+++ ProyectoVisual/Frog/src/components/MovementComponentBomb.cpp
@@ -3,12 +3,12 @@
 #include "../ecs/Component.h"
 #include "../scenes/RoomScene.h"
 
-MovementComponentBomb::MovementComponentBomb() : isLaunched(true), direction(DOWN) {
+MovementComponentBomb::MovementComponentBomb() : isLaunched(true), direction(NONE) {
 }
 
 void MovementComponentBomb::initComponent() {
 	//Obtenemos componentes
-	moveFrog = static_cast<MovementComponentFrog*>(ent->getScene()->getPlayer()->getComponent(TRANSFORM_COMPONENT));
+	moveFrog = static_cast<MovementComponentFrog*>(ent->getScene()->getPlayer()->getComponent(MOVEMENT_COMPONENT));
 	animator = static_cast<AnimationComponent*>(ent->getComponent(ANIMATION_COMPONENT));
 	tr = static_cast<TransformComponent*> (ent->getComponent(TRANSFORM_COMPONENT));
 }
@@ -16,31 +16,34 @@ void MovementComponentBomb::initComponent() {
 //Mueve la bomba en la direccion dada:
 void MovementComponentBomb::moveBomb(Directions dir) {
 	switch (dir) {
-	case DOWN:
-		tr->setCasilla(tr->getCasilla() + Vector2D(0, 1));
+	case Directions::DOWN:
+		tr->setCasilla(tr->getCasilla() + Vector2D(0, 0.005));
+		std::cout << "BOMBA SE ABAJO" << std::endl;
 		break;
-	case UP:
-		tr->setCasilla(tr->getCasilla() + Vector2D(0, -1));
+	case Directions::UP:
+		tr->setCasilla(tr->getCasilla() + Vector2D(0, -0.005));
+		std::cout << "BOMBA SE ARRIBA" << std::endl;
 		break;
-	case LEFT:
-		tr->setCasilla(tr->getCasilla() + Vector2D(-1, 0));
+	case Directions::LEFT:
+		tr->setCasilla(tr->getCasilla() + Vector2D(-0.005, 0));
+		std::cout << "BOMBA SE IZQDA" << std::endl;
 		break;
-	case RIGHT:
-		tr->setCasilla(tr->getCasilla() + Vector2D(1, 0));
+	case Directions::RIGHT:
+		tr->setCasilla(tr->getCasilla() + Vector2D(0.005, 0));
+		std::cout << "BOMBA SE DERECHA" << std::endl;
 		break;
 	default:
 		break;
 	}
 	isLaunched = false;
 	animator->playAnimation("BOMB_IDLE");
-	std::cout << "BOMBA SE MUEVE" << std::endl;
 }
 
 
 void MovementComponentBomb::update() {
-	if (isLaunched) //Si se activa la flag de que se ha lanzado la bomba
+	if (isLaunched) { //Si se activa la flag de que se ha lanzado la bomba
 		direction = moveFrog->getDirection(); //Obtenemos direccion a la que mira la rana
-
+	}
 	moveBomb(direction); //movemos bomba en la dirección dada
 }
 
