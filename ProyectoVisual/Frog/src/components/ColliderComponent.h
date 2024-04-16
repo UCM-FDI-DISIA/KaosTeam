1d96b338bee8c9ee558925b9ab14de3eca7f3642
Diego Montenegro <dmonoficial22@gmail.com>
2024-04-16 14:49:36 +0200

    mas cositas

--- ProyectoVisual/Frog/src/components/ColliderComponent.h
+++ ProyectoVisual/Frog/src/components/ColliderComponent.h
@@ -4,16 +4,8 @@
 #include <functional>
 
 //Comprueba colisiones de objetos, y envía un mensaje con la información relevante a los componentes suscritos
-//La llamada a comporbar colisiones debe hacerse en el update del room antes o después del update de las entidades
-
-/*Flonk tiene un collision component que en su update comprueba la interseccion de su SDLrect con el SDLrect de el resto de entidades colisionables.
-- Los SDLrects estos se crean en cada iteración cogiendo la posición en MovementComponent y el width y height de la Textura * en RenderComponent.Feo pero cuando tengamos un componente transform ya se gestionará mejor.
-- Las entidades colisionables son las que tengan un componente collideable(igual no se llama así), este componente recibe un método callback en su constructora al que se le llamará si Flonk detecta una collisión con el SDLrect de esta entidad.
-De esta manera, provisional para este hito 2, flonk va a ser el que gestione con qué entidad ha collisionado y 
-llamará al metodo callback que corresponda, es un for simplemente y así Diego implementa esto y la 
-gente que ha estado haciendo enemigos puede hacer su método onCollision y meterlo en las constructoras y fuera : )
-*/
-//Es decir, al refactorizar en el hito 3 van a cambiar muchas cosas
+//NUEVA VERSION: El collider component puede tener varios colliders. Uno siempre que va a estar establecido por el transform.
+//La entidad puede tener mas cajas de colisiones, que se deberán añadir desde otros componentes.
 class ColliderComponent : public Component
 {
 public:
@@ -26,10 +18,8 @@ public:
 	//A llamar en caso de colisión, como parametro la entidad colisionada
 	void OnCollision(Entity* e);
 
-	//Añade la funcion a llamar cuando hay colision
-	//En el futuro molaria poder almacenas muchas funciones y llamar a todas
+	//Añade una funciona al collider que quieras
 	void AddCall(std::function<void(Entity* e)>* func);
-	
 private:
 	//Lista de funciones a llamar cuando se detecta la colisión
 	std::list<std::function<void(Entity*)>*> funciones;
