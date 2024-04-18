94cd35ac0a20d4a444d736faa58ad8ebc4f096d7
mikeletzu <120392721+mikeletzu@users.noreply.github.com>
2024-04-18 15:24:22 +0200

    Mapa zona final (restaurante)

--- ProyectoVisual/Frog/src/scenes/RoomScene.h
+++ ProyectoVisual/Frog/src/scenes/RoomScene.h
@@ -45,8 +45,9 @@ private:
 public:
 	RoomScene(int id) : id(id) {
 		//A trav�s del id de la sala, se deben buscar los datos necesarios para cargar el tilemap y las entidades de la sala.
-		mapReader = new MapManager("resources/maps/niveles/nivel01/mapaN1_01.tmx", this);
-		mapReader->loadObj("resources/maps/niveles/nivel01/mapaN1_01.tmx");
+		std::string initMapPath = "resources/maps/niveles/nivelFinal/mapaNF.tmx";
+		mapReader = new MapManager(initMapPath, this);
+		mapReader->loadObj(initMapPath);
 
 		//Create player desde el mapa
 		cameraManager = Camera::instance();
@@ -54,70 +55,8 @@ public:
 		HUD = HUDManager::GetInstance();
 		shopManager = Shop::instance();
 		shopManager->setPlayer(player);
-
-#pragma region Cosas q vamos a borrar pronto
-		//Texture* textFly = new Texture(sdlutils().renderer(), "../Frog/resources/sprites/moscaSpritesheet.png", 1, 3);
-
-		//Entity* fly = new Entity(this);
-
-		//TransformComponent* transform = new TransformComponent();
-		//AnimationComponent* animFly = new AnimationComponent();
-		//RenderComponent* rndrFly = new RenderComponent(textFly, 1, 3, 0.5, animFly);
-
-		//rndrFly->setContext(fly);
-
-		//Animation a; //Animaciones mosca
-		//a = Animation({ Vector2D(0,0), Vector2D(0,1) }, false, true);
-		//animFly->addAnimation("FLY", a);
-
-		//fly->addRenderComponent(rndrFly);
-		//fly->addComponent(ANIMATION_COMPONENT, animFly);
-
-		//animFly->playAnimation("FLY");
-
-
-		//MovementComponent* mvm = new MovementComponentFly(Vector2D(0, 3));
-		//mvm->setContext(fly);
-		//fly->addComponent(MOVEMENT_COMPONENT, mvm);
-
-		//entityList.push_back(fly);
-
-
-		//Entity* flyToPlayer = new Entity(this);
-
-		//FollowPlayerComponent* fpc = new FollowPlayerComponent(Vector2D(0, 0));
-		//fpc->setContext(flyToPlayer);
-		//flyToPlayer->addComponent(MOVEMENT_COMPONENT, fpc);
-
-		//AnimationComponent* animFly2 = new AnimationComponent();
-		//RenderComponent* rndrFly2 = new RenderComponent(textFly, 1, 3, 0.5, animFly2);
-		//rndrFly2->setContext(flyToPlayer);
-
-		//a = Animation({ Vector2D(0,0), Vector2D(0,1) }, false, true);
-		//animFly2->addAnimation("FLY", a);
-
-		//flyToPlayer->addRenderComponent(rndrFly2);
-		//flyToPlayer->addComponent(ANIMATION_COMPONENT, animFly2);
-
-		//animFly2->playAnimation("FLY");
-
-		////Collider al flyToPlayer para probar las colisiones
-		//ColliderComponent* collider = new ColliderComponent();
-		//collider->setContext(flyToPlayer);
-		//flyToPlayer->addComponent(COLLIDER_COMPONENT, collider);
-
-		//entityList.push_back(flyToPlayer);
-		//createFish(Vector2D(0, 3), 4);
-		//entityList.push_back(flyToPlayer);
-
-
-		//createFish(Vector2D(0, 3), 4);
-		
-
-#pragma endregion
-
-		
 	};
+
 	void AddEntity(Entity* entity);
 	void render() override;
 	void update() override;
