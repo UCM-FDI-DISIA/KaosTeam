8e9542a92bafcb1e4e26bc4b9f3137a3da85ed80
ADDI01 <dapala02@ucm.es>
2024-05-02 19:17:09 +0200

    Cambiado variable render de BossComponent, añadido metedo render para los avisos de los cubiertos, y cambiados update generateCutlery, createCutlery, moveCutlery

--- ProyectoVisual/Frog/src/components/BossComponent.cpp
+++ ProyectoVisual/Frog/src/components/BossComponent.cpp
@@ -3,7 +3,7 @@
 #include "../scenes/RoomScene.h"
 
 
-BossComponent::BossComponent() : currState(MOVE), shadowTimer(0), postAttackTimer(2), //
+BossComponent::BossComponent() : currState(MOVE), attackStartTime(0), postAttackTimer(2), //
 							aviso(&sdlutils().images().at("aviso")) //
 {
 	texturasCubiertos = new Texture*[MAX_CUBIERTOS];
@@ -20,7 +20,7 @@ BossComponent::~BossComponent()
 void BossComponent::initComponent()
 {
 	tr = static_cast<TransformComponent*>(ent->getComponent(TRANSFORM_COMPONENT));
-	render = ent->getRenderComponent();
+	rnd = ent->getRenderComponent();
 	mov = static_cast<MovementComponentFrancois*>(ent->getComponent(MOVEMENT_COMPONENT));
 	coll = static_cast<ColliderComponent*>(ent->getComponent(COLLIDER_COMPONENT));
 	coll->GetTransformCollider()->AddCall([this](Entity* e, Collider c) {attack(e, c); }); //A�adimos callback
@@ -29,14 +29,27 @@ void BossComponent::initComponent()
 
 void BossComponent::update()
 {
-	moveCutlery();
+	if(poolCubiertos.empty())
+		moveCutlery();
+}
+
+void BossComponent::render()
+{
+	for (int i = 0; i < poolCubiertos.size(); i++) {
+
+	}
 }
 
 void BossComponent::generateCutlery()
 {
-	int rand = sdlutils().rand().nextInt(1 + contDishes, 5 + contDishes); //Cuantos cubiertos tendra el ataque
-	for (int i = 0; i < rand; i++) {
+	int numCutlery = sdlutils().rand().nextInt(1 + contDishes, 5 + contDishes); //Cuantos cubiertos tendra el ataque
+	for (int i = 0; i < numCutlery; i++) {
 		int c = sdlutils().rand().nextInt(CUCHARA, TENEDOR + 1); //Se decide que cubierto se añade a la pool
+		int x = sdlutils().rand().nextInt(tr->getCasilla().getX(), 
+			tr->getWidth() / ent->getScene()->getMapReader()->getTileSize() + 1); //Definde x random donde ira el cubierto
+		poolCubiertos.emplace_back(cubiertos[c], true);
+		poolCubiertos.back().first->tr_->setCasilla(Vector2D(x, -1)); //Ponemos el cubierto una casilla fuera de la pantalla
+		
 	}
 
 }
@@ -54,9 +67,10 @@ void BossComponent::cleanPool()
 void BossComponent::attack(Entity* e, Collider c)
 {
 	if (e->getName() == FROG_ENTITY) {
-		std::cout << "RANANANANANAN";
+		//std::cout << "RANANANANANAN"; //Para comprobar si se llama al callback
+		generateCutlery();
+		attackStartTime = sdlutils().currRealTime();
 	}
-	generateCutlery();
 }
 
 void BossComponent::createCutlery()
@@ -68,7 +82,12 @@ void BossComponent::createCutlery()
 	Cubierto* c = &aux;
 	for (int i = 0; i < MAX_CUBIERTOS; i++) {
 		cubiertos.push_back(c);
+
 		cubiertos[i]->tipo_ = (tipoCubierto)i; //Asignamos id con el tipo de cubierto
+		cubiertos[i]->speed_ = Vector2D(0, -0.1); //Setteamos su velocidad
+		if (i != 0) cubiertos[i]->spawnTime_ = sdlutils().rand().nextInt(0, 3) + cubiertos[i - 1]->spawnTime_;
+		else cubiertos[i]->spawnTime_ = sdlutils().rand().nextInt(0, 2);
+
 		cubiertos[i]->ent_ = new Entity(ent->getScene()); //Creamos entidad cubierto
 		cubiertos[i]->tr_ = new TransformComponent(Vector2D(i , i), 80, 160); //Añadimos transform al cubierto
 		cubiertos[i]->ent_->addComponent(TRANSFORM_COMPONENT, cubiertos[i]->tr_);
@@ -77,22 +96,20 @@ void BossComponent::createCutlery()
 		cubiertos[i]->render_ = new RenderComponent(texturasCubiertos[i]); //Añadimos la textura pertinente al render
 		cubiertos[i]->ent_->addRenderComponent(cubiertos[i]->render_);
 		ent->getScene()->AddEntity(cubiertos[i]->ent_); //Añadimos a la lista de entidades
-		cubiertos[i]->speed_ = Vector2D(0, -0.1); //Setteamos su velocidad
 	}
 }
 
 void BossComponent::moveCutlery()
 {
 	//L�gica para mover los cubiertos que est�n activos en la pool de cubiertos
-	for (auto cubierto : poolCubiertos) {
-		if (cubierto.second) {
+	for (pair cubierto : poolCubiertos) {
+		if (cubierto.second && attackStartTime + cubierto.first->spawnTime_ >= sdlutils().currRealTime()) {
 			cubierto.first->tr_->setOffset(cubierto.first->tr_->getOffset() + cubierto.first->speed_);
 			cubierto.first->tr_->setCasilla(cubierto.first->speed_ + cubierto.first->tr_->getCasilla());
 			cubierto.first->tr_->setOffset({ -cubierto.first->tr_->getCasilla().getX() / 2,
 				-cubierto.first->tr_->getCasilla().getY() / 2 });
 		}
-	}
-		
+	}		
 }
 
 void BossComponent::onCutleryCollision(Entity* e, Collider c)
--- ProyectoVisual/Frog/src/components/BossComponent.h
+++ ProyectoVisual/Frog/src/components/BossComponent.h
@@ -10,11 +10,11 @@
 #include "MovementComponent.h"
 #include "MovementComponentFrancois.h"
 #include <vector>
+#include <utility>
 
-const int MAX_TIME_ON_SHADOW = 3;
 const int MAX_CUBIERTOS = 4;
 
-enum bossState { MOVE, DETECT, ATTACK };
+enum bossState { MOVE, ATTACK };	
 enum tipoCubierto { CUCHARA, CUCHILLO, CUCHILLO_CARNICERO, TENEDOR };
 struct Cubierto {
 	Entity* ent_;
@@ -24,19 +24,20 @@ struct Cubierto {
 	ColliderComponent* coll_;
 	Vector2D speed_;
 	int spawnTime_; //Tiempo en spawnear tras iniciar el ataque del Boss
+	SDL_Rect dest; //Lugar donde va el aviso para avisar de que un cubierto atravesara esa fila
 };
 
 class BossComponent: public Component
 {	
 private:
 	bossState currState;
-	RenderComponent* render;
+	RenderComponent* rnd;
 	std::vector<Cubierto*> cubiertos;
 	std::vector<std::pair<Cubierto*, bool>> poolCubiertos; //El numero de cubiertos max instanciados depende de la zona
 	Texture** texturasCubiertos; //Array de texturas de los cubiertos
 
-	int shadowTimer; //para saber cuanto tiempo ha pasado en la sombra
-	int postAttackTimer; //para evitar que empiece a detectar tras un ataque
+	Uint32 attackStartTime; //Definde en que momento empieza un ataque
+	Uint32 postAttackTimer; //para evitar que empiece a detectar tras un ataque
 	int contDishes; //Platos contaminados en cada momento
 
 	Texture* aviso; //Imagen aviso en que columna aparecera un cubierto
@@ -50,6 +51,7 @@ public:
 
 	void initComponent() override;
 	void update() override;
+	void render() ;
 
 	void attack(Entity* e, Collider c);
 
