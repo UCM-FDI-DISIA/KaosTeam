1d873c0500d0489ee3a1db79e95a2ab0bd1c5b9f
Chao <114423476+ChaoIsBalling@users.noreply.github.com>
2024-05-06 16:04:17 +0200

    EndingScene creada

---
+++ ProyectoVisual/Frog/src/scenes/EndingScene.cpp
@@ -0,0 +1,17 @@
+#include "EndingScene.h"
+
+EndingScene::EndingScene(Game* g, GameState* nGS):currBg(0)
+{
+}
+
+EndingScene::~EndingScene()
+{
+}
+
+void EndingScene::render()
+{
+}
+
+void EndingScene::update()
+{
+}
